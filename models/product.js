e1a6f86722a0a5e6d4baba770b8c500b86298c26
pk381 <kumarprabhat0023@gmail.com>
2023-07-29 23:37:26 +0530

    sql

--- models/product.js
+++ models/product.js
@@ -1,13 +1,6 @@
-const fs = require('fs');
-const path = require('path');
+const db = require("../util/database");
 
-const p = path.join(
-  path.dirname(process.mainModule.filename),
-  'data',
-  'products.json'
-);
-
-const getProductsFromFile = cb => {
+const getProductsFromFile = (cb) => {
   fs.readFile(p, (err, fileContent) => {
     if (err) {
       cb([]);
@@ -25,25 +18,10 @@ module.exports = class Product {
     this.price = price;
   }
 
-  save() {
-    this.id = Math.random().toString();
-    getProductsFromFile(products => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
-        console.log(err);
-      });
-    });
-  }
-
-  static findById(id, cb){
-    getProductsFromFile(products =>{
-
-      const product = products.find(p => p.id === id);
-      cb(product);
-    })
-  }
+  save() {}
 
   static fetchAll(cb) {
-    getProductsFromFile(cb);
+    return db.execute("SELECT * FROM products");
   }
+  static findById(id, cb) {}
 };
