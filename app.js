95e5ec928e7839f9ad4ee246fed5d710ffeea403
pk381 <kumarprabhat0023@gmail.com>
2023-08-04 22:58:12 +0530

    cart added

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
+
+
 
 const app = express();
 
@@ -40,6 +44,9 @@ Product.belongsTo(User, {
 });
 
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsToMany(Product, {through: CartItem});
+Product.belongsToMany(Cart, { through: CartItem});
 
 sequelize.sync().then( res =>{
 
