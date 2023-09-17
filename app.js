dc80f9a46fd398d73c383903a532898d7841bb1b
pk381 <kumarprabhat0023@gmail.com>
2023-09-17 10:50:51 +0530

    cleaning previous project

--- app.js
+++ app.js
@@ -1,68 +1,53 @@
-const path = require('path');
-
 const express = require('express');
-const bodyParser = require('body-parser');
+const path = require('path');
+const bodyParser = require('body-parser'); 
 
-const errorController = require('./controllers/error');
-
-const sequelize = require('./util/database');
-const Product = require('./models/product');
-const User = require('./models/user');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cart-item');
-
-
+// database
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+// tables
+const User = require('./mongoModels/user');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+// routes
+const userRoute = require('./routes/user');
+const expensRoute = require('./routes/expense');
+// const purchaseRoute = require('./routes/purchase');
+// const premiumRoute = require('./routes/premium');
+// const forgotPasswordRoute = require('./routes/forgot_password');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+
+// static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next)=>{
-    User.findById(1)
-    .then(user=>{
-        req.user = user;
-        next();
-    }).catch(err=>{
-        console.log(err);
-    })
-})
+app.use(bodyParser.json());
 
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+app.use('/user',userRoute);
+app.use('/expense',expensRoute);
+// app.use('/premium',premiumRoute);
 
-app.use(errorController.get404);
+// app.use(forgotPasswordRoute);
+// app.use('/purchase',purchaseRoute);
 
-Product.belongsTo(User, {
-    constraint: true, onDelete: 'CASCADE'
+// serving index page
+app.use('/',(req, res, next)=>{
+    res.redirect("/user/sign_up");
 });
 
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsToMany(Product, {through: CartItem});
-Product.belongsToMany(Cart, { through: CartItem});
+// mongoConnect(async ()=>{
+    
+//     console.log("connected");
+//     app.listen(4000);
+// });
 
-sequelize.sync().then( res =>{
+mongoose.connect('mongodb+srv://prabhat:prabhat@cluster0.yktjrxl.mongodb.net/expensetracker?retryWrites=true&w=majority')
+.then(()=>{
+    console.log('connected');
+    app.listen(4000);
 
-   return User.findById(1);
-  
 })
-.then(user=>{
-    if(!user)
-        return User.create({name: 'Max', email: 'test@.com'});
-
-    return user;
-}).then(user=>{
-    console.log(user);
-    app.listen(3000);
-}).catch( err =>{
-    console.log();
+.catch(err =>{
+    console.log(err);
 })
-
 
